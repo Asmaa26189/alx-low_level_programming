5397bf25126483de6c5e5b6c3a02de461cb835bf
Asmaa26189 <https://github.com/Asmaa26189>
2023-04-21 17:19:38 -0700

    0x01 update 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,10 @@
 */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if(n<0)
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if(n<0)
     {
       printf("%d is negative\n", n);
     }else if(n>0)
@@ -20,7 +20,7 @@ int main(void)
      printf("%d is positive\n", n);
     } else
     {
-      printf("%d is is zero\n", n);
+      printf("%d is zero\n", n);
     }
-  return (0);
+    return (0);
 }
