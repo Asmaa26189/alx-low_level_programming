9d29dcdc059739ce9e25893db0d802d4dc9b3ad3
Asmaa26189 <https://github.com/Asmaa26189>
2023-04-25 14:28:32 -0700

    test first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+
+int main(void)
+  {
+
+      printf("_putchar\n")
+
+      return 0
+
+  }
