2ec0be6e9a59cded1ff2f817f54e0004b828b4c1
Asmaa26189 <https://github.com/Asmaa26189>
2023-04-25 18:26:16 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "6-abs.c"
+/**
+ * print_last_digit - main function
+ *
+ * @n: the number to get last digit 
+ *
+ * Return: last digit of n number
+ *
+ */
+
+int print_last_digit(int n)
+{
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
+}
