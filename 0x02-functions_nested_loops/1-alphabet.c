1b956c98145d1bda80d112bf652b6cc756ced038
Asmaa26189 <https://github.com/Asmaa26189>
2023-04-25 17:23:00 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabet
+ *
+ *Return: always void;
+ *
+ */
+void print_alphabet(void)
+{
+  for (char i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
