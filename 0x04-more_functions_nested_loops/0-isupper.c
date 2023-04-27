96e80550ef5f2e21d223c2c879cd5f1dc0f6ef56
Asmaa26189 <https://github.com/Asmaa26189>
2023-04-27 05:10:46 -0700

    task 0 update 2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _isupper - checks capital or small
- * @c: character 
  *
- * Return: 1 if c is uppercase or 0 otherwise
+ * Return: 1 if c is uppercase or 0 otherwise;
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
