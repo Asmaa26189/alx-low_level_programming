bf45084cf83af0e757b9a727e1c5ba43ef85d56b
Asmaa26189 <https://github.com/Asmaa26189>
2023-05-14 05:51:14 -0700

    task0

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - determine if digit
+ *
+ * @c: character
+ *
+ * Return: boolean
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - character string
+ *
+ * @s: character
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
