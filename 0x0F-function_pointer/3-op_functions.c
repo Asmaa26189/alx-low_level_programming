bdff43c001e2e7da5b15ab4905be113aa9d90dbf
Asmaa26189 <https://github.com/Asmaa26189>
2023-05-24 15:16:19 -0700

    tasks

---
+++ 0x0F-function_pointer/3-op_functions.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - add
+ * @a: int
+ * @b: int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract
+ * @a: int
+ * @b: int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplie
+ * @a: int
+ * @b: int
+ * Return: times
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide
+ * @a: int
+ * @b: int
+ * Return: divide
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: int
+ * @b: int
+ * Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
