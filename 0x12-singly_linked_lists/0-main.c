a1430cc7af9d9bc16656588855f3191ce012aaf9
Asmaa Hesham <108579670+Asmaa26189@users.noreply.github.com>
2023-06-01 14:25:19 +0300

    update taskss

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -4,9 +4,9 @@
 #include "lists.h"
 
 /**
- * main - check the code
+ * main - main
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
@@ -19,8 +19,8 @@ int main(void)
     new = malloc(sizeof(list_t));
     if (new == NULL)
     {
-            printf("Error\n");
-            return (1);
+        printf("Error\n");
+        return (1);
     }
     new->str = strdup("Hello");
     new->len = 5;
