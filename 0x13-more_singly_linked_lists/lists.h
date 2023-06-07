93339a030788d4bcb9599816711da95c84c2755e
Asmaa Hesham <108579670+Asmaa26189@users.noreply.github.com>
2023-06-07 15:30:46 +0300

    task 13 6

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,7 +24,7 @@ typedef struct list_s
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure*reverse_listint
  * for Holberton project
  */
 typedef struct listint_s
@@ -48,4 +48,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+
 #endif
