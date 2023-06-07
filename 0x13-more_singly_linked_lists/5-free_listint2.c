8640b3f33e4ddc2df36f656fda0907603b3bac07
Asmaa Hesham <108579670+Asmaa26189@users.noreply.github.com>
2023-06-07 15:13:17 +0300

    tasks 13 6

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,26 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
-	if (head == NULL)
+	if (!head)
 		return;
-	while (*head != NULL)
+	if (*head)
 	{
-		tmp = *head;
-		free(*head);
-		*head = tmp->next;
+		temp = *head;
+		*head = NULL;
+		free_listint(temp);
+		return;
 	}
-	*head = NULL;
+}
+/**
+ * free_listint - free list
+ * @head: head
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
 }
