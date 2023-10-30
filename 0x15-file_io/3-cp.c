47c3531fec092ca4d7382047fa01d550ee81870f
Asmaa26189 <engasmaahesham@gmail.com>
2023-10-30 23:50:08 +0200

    30-10-2023

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,16 +11,14 @@ int main(int ac, char **av)
 	int openFile, openFileToo, writeFile, readFile;
 
 	if (ac != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	if (ac != 3)
 		exit(97);
-	}
 	openFile = open(av[1], O_RDONLY);
 	if (openFile == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+	if (openFile == -1)
 		exit(98);
-	}
 	openFileToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (openFileToo == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -30,10 +28,9 @@ int main(int ac, char **av)
 	{
 		writeFile = write(openFileToo, buff, readFile);
 		if (writeFile == -1)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		if (writeFile == -1)
 			exit(99);
-		}
 	}
 	if (readFile == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
